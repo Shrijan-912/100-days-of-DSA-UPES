0969756695599981cbb6292ddbaa12375a6bb579
Shrijan-912 <singhshrijan7@gmail.com>
2026-02-15 23:11:19 +0530

    Add files via upload

---
+++ Q12.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+
+    printf("Enter matrix elements:\n");
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // First condition: must be square
+    if(m != n) {
+        printf("Not a Symmetric Matrix");
+        return 0;
+    }
+
+    // Check symmetry
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(matrix[i][j] != matrix[j][i]) {
+                printf("Not a Symmetric Matrix");
+                return 0;
+            }
+        }
+    }
+
+    printf("Symmetric Matrix");
+
+    return 0;
+}
