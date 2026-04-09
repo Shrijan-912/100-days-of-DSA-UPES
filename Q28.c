9cc03f7948542b9ba06add8b78a18b63e482a6dc
Shrijan-912 <singhshrijan7@gmail.com>
2026-04-09 23:15:45 +0530

    Add files via upload

---
+++ Q28.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create circular linked list
+struct Node* createCircularList(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for (int i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Make it circular
+    if (temp != NULL)
+        temp->next = head;
+
+    return head;
+}
+
+// Function to traverse circular linked list
+void traverse(struct Node* head) {
+    if (head == NULL) return;
+
+    struct Node* temp = head;
+
+    do {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node* head = createCircularList(n);
+
+    traverse(head);
+
+    return 0;
+}
